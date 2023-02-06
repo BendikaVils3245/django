4e7810a96b3b7c95cf0d2996389b071eb54e9f47
Misha <BendikaVils@bk.ru>
2023-02-06 17:51:33 +0300

    sled_commit

--- crm/views.py
+++ crm/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Order
-
+from .forms import OrderForms
 
 # Create your views here.
+#Выводит весь query set
 def first_page(request):
     object_list = Order.objects.all()
-    return render(request,'./index.html', {'object_list':object_list})
+    form = OrderForms()
+    return render(request,'./index.html', {'object_list':object_list,
+                                           'form':form})
+
+#Отправка формы и заносит её в БД
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name = name,order_phone = phone)
+    element.save()
+    return render(request,'./thanks_page.html',{'name':name,
+                                                'phone': phone})
